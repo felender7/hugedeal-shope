56e394a9d4104045263549f0c7c7260b1676dc88
Felender Hlungwani <felender7@gmail.com>
2019-11-06 18:19:36 +0200

    add additional resouces

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,7 +1,9 @@
 class Spree::StoreController
    before_action :load_taxonomies
 
+
    def load_taxonomies
        @taxonomies = Spree::Taxonomy.includes(root: :children)
    end
+   
 end
--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -10,51 +10,70 @@ module Spree
       end.compact
     end
   end
+end
 
 
-  module Spree::BaseHelper
-        def logo(image_path: Spree::Config[:logo], img_options: {})
-          link_to image_tag(image_path, img_options), spree.root_path
-        end
+module Spree::BaseHelper
+    def logo(image_path: Spree::Config[:logo], img_options: {})
+      link_to image_tag(image_path, img_options), spree.root_path
+    end
 
-        def nav_tree(root_taxon, current_taxon, max_level = 1)
-          return '' if max_level < 1 || root_taxon.children.empty?
-          content_tag :ul, class: 'list-links' do
-            taxons = root_taxon.children.map do |taxon|
-              css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? '' : nil
-              content_tag :li, class: css_class do
-               link_to(taxon.name, seo_url(taxon), class: "") +
-                 taxons_tree(taxon  ,current_taxon, max_level - 1)
-              end
-            end
-            safe_join(taxons, "\n")
-
+    def nav_tree(root_taxon, current_taxon, max_level = 1)
+      return '' if max_level < 1 || root_taxon.children.empty?
+      content_tag :ul, class: 'list-links' do
+        taxons = root_taxon.children.map do |taxon|
+          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? '' : nil
+          content_tag :li, class: css_class do
+            link_to(taxon.name, seo_url(taxon), class: "") +
+            taxons_tree(taxon  ,current_taxon, max_level - 1)
           end
         end
+        safe_join(taxons, "\n")
 
-        def taxon_breadcrumbs(taxon, separator = '&nbsp;&raquo;&nbsp;', breadcrumb_class = 'list-inline')
-          return '' if current_page?('/') || taxon.nil?
+      end
+    end
 
-          crumbs = [[t('spree.home'), spree.root_path]]
+    def taxon_breadcrumbs(taxon, separator = '&nbsp;&raquo;&nbsp;', breadcrumb_class = 'list-inline')
+      return '' if current_page?('/') || taxon.nil?
 
-          crumbs << [t('spree.products'), products_path]
-          if taxon
-            crumbs += taxon.ancestors.collect { |a| [a.name, spree.nested_taxons_path(a.permalink)] } unless taxon.ancestors.empty?
-            crumbs << [taxon.name, spree.nested_taxons_path(taxon.permalink)]
-          end
+      crumbs = [[t('spree.home'), spree.root_path]]
 
-          separator = raw(separator)
+      crumbs << [t('spree.products'), products_path]
+      if taxon
+        crumbs += taxon.ancestors.collect { |a| [a.name, spree.nested_taxons_path(a.permalink)] } unless taxon.ancestors.empty?
+        crumbs << [taxon.name, spree.nested_taxons_path(taxon.permalink)]
+      end
 
-          items = crumbs.each_with_index.collect do |crumb, i|
-            content_tag(:li, itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem', class:"") do
-              link_to(crumb.last, itemprop: 'item') do
-                content_tag(:span, crumb.first, itemprop: 'name') + tag('meta', { itemprop: 'position', content: (i + 1).to_s }, false, false)
-              end + (crumb == crumbs.last ? '' : separator)
-            end
-          end
+      separator = raw(separator)
 
-          content_tag(:nav, content_tag(:ol, raw(items.map(&:mb_chars).join), class: breadcrumb_class, itemscope: '', itemtype: 'https://schema.org/BreadcrumbList'), id: 'breadcrumb', class: 'breadcrumb')
+      items = crumbs.each_with_index.collect do |crumb, i|
+        content_tag(:li, itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem', class:"") do
+          link_to(crumb.last, itemprop: 'item') do
+            content_tag(:span, crumb.first, itemprop: 'name') + tag('meta', { itemprop: 'position', content: (i + 1).to_s }, false, false)
+          end + (crumb == crumbs.last ? '' : separator)
         end
+      end
+
+      content_tag(:nav, content_tag(:ol, raw(items.map(&:mb_chars).join), class: breadcrumb_class, itemscope: '', itemtype: 'https://schema.org/BreadcrumbList'), id: 'breadcrumb', class: 'breadcrumb')
+    end
+
+    def layout_partial
+      if controller.devise_controller?
+        'spree/base/devise'
+      else
+        'spree/base/application'
+      end
+    end
+
+    def flash_messages(opts = {})
+      ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+
+      flash.each do |msg_type, text|
+        unless ignore_types.include?(msg_type)
+          concat(content_tag :div, text, class: "flash alert alert-#{msg_type}")
+        end
+      end
+      nil
+    end
 
   end
-end
